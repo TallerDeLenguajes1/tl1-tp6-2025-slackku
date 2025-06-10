4c8821cb75d76a51b114706012d1f75957d0555e
slacku <bananuya3@gmail.com>
2025-06-10 11:41:08 -0300

    Ejercicio 2 terminado

--- Program.cs
+++ Program.cs
@@ -1,23 +1,71 @@
-﻿Console.WriteLine("Hello, World!");
-int a;
-int b;
-a = 10;
-b = a;
-Console.WriteLine("valor de a:" + a);
-Console.WriteLine("valor de b:" + b);
+﻿using System.Runtime.InteropServices;
 
-Console.Write("Ingrese un numero: ");
-string input = Console.ReadLine();
-int number;
-if (int.TryParse(input, out number))
+Console.Write("Operaciones aceptadas:");
+Console.Write("\x1b[1m + - * /\x1b[0m\n");
+Console.Write(" └┬ Ingrese la operacion que desea hacer: ");
+string inputOp = Console.ReadLine();
+bool calcular = true;
+int num1, num2;
+while (calcular)
 {
-    Console.WriteLine(number);
-    if (number > 0)
+    bool rightOp = !inputOp.Equals("*") || !inputOp.Equals("/") || !inputOp.Equals("-") || !inputOp.Equals("+");
+    while (!rightOp)
     {
-        Console.Write("El inverso del numero ingresado es: {0:N9}", Decimal.Divide(1, number));
+        Console.Write(" └┬ Operacion Erronea, ingrese una operacion valida:  ");
+        inputOp = Console.ReadLine();
+        rightOp = !inputOp.Equals("*") || !inputOp.Equals("/") || !inputOp.Equals("-") || !inputOp.Equals("+");
     }
-    else
+
+    if (inputOp == "*" || inputOp == "/" || inputOp == "-" || inputOp == "+")
     {
-        Console.WriteLine("La division por cero no esta definida.");
+        Console.Write("  └┬ Ingrese el primer operando: ");
+        string inputNum1 = Console.ReadLine();
+        bool isInputNum1Ok = int.TryParse(inputNum1, out num1);
+        if (!isInputNum1Ok)
+        {
+            while (!isInputNum1Ok)
+            {
+                Console.Write("  └┬ Error, ingrese un numero valido: ");
+                inputNum1 = Console.ReadLine();
+                isInputNum1Ok = int.TryParse(inputNum1, out num1);
+            }
+        }
+        Console.Write("   ├ Ingrese el segundo operando: ");
+        string inputNum2 = Console.ReadLine();
+        bool isInputNum2Ok = int.TryParse(inputNum2, out num2);
+        if (!isInputNum2Ok)
+        {
+            while (!isInputNum2Ok)
+            {
+                Console.Write("  └┬ Error, ingrese un numero valido: ");
+                inputNum2 = Console.ReadLine();
+                isInputNum2Ok = int.TryParse(inputNum2, out num2);
+            }
+        }
+        float resultado;
+        switch (inputOp)
+        {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+            default:
+                resultado = 0;
+                break;
+        }
+        Console.WriteLine($"   └ El resultado de la operacion es: {resultado}");
+        Console.Write("¿Quiere seguir realizando calculos? (Y/N): ");
+        string buff = Console.ReadLine();
+        calcular = buff.ToUpper().Equals("Y");
+        Console.Write(" └┬ Ingrese la operacion que desea hacer: ");
+        inputOp = Console.ReadLine();
     }
 }
