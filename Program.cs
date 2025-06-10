ef5e2f58c1a93f4d256b460e45aca1689afc9842
slacku <bananuya3@gmail.com>
2025-06-10 00:53:12 -0300

    Primer punto terminado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hello, World!");
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+
+Console.Write("Ingrese un numero: ");
+string input = Console.ReadLine();
+int number;
+if (int.TryParse(input, out number))
+{
+    Console.WriteLine(number);
+    if (number > 0)
+    {
+        Console.Write("El inverso del numero ingresado es: {0:N9}", Decimal.Divide(1, number));
+    }
+    else
+    {
+        Console.WriteLine("La division por cero no esta definida.");
+    }
+}
